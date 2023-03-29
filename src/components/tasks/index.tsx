761e03a73ff1ae12ae4327b4e2013fe5db0ce47a
Thiago Carvalho <thiagoafonso07@hotmail.com>
2023-03-29 11:49:19 -0300

    📲

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Switch } from 'react-native';
 import { styles } from './styles';
 
 export function Tasks() {
+	const [isEnabled, setIsEnabled] = useState(false);
+	const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
 	return (
 		<View style={styles.View}>
-			<Text>hello</Text>
+			<Text>placeholder</Text>
+			<Switch
+				trackColor={{ false: '#A1A1A1', true: '#31CF67' }}
+				thumbColor={'#FFFFFF'}
+				ios_backgroundColor="#A1A1A1"
+				onValueChange={toggleSwitch}
+				value={isEnabled}
+			/>
 		</View>
 	);
 }
