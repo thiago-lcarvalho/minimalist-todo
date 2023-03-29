43ee3303bb3a52ceb557ccc964ddefe0b84a0b6e
Thiago Carvalho <thiagoafonso07@hotmail.com>
2023-03-29 09:44:19 -0300

    ✅ Ok

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import { styles } from './styles';
 
 
-export default function Home() {
+export function Home() {
+
 	return (
-		<View
+		    <View
 			style={styles.container}
-		>
-			<Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+            >
+			<Text style={styles.text}>
+                Nome do evento
+            </Text>
+            <Text style={styles.textDate}>
+                Sexta, 28 de março de 2023.
+            </Text>
+            <TextInput 
+                placeholder= 'hello'
+                placeholderTextColor= "#6B6B6B6B"
+                style= {styles.input}/>
 
 			<StatusBar style="auto" />
 		</View>
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -4,12 +4,20 @@ export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-    fontWeight: 'bold',
+        fontWeight: 'bold',
+        justifyContent: 'center'
 	},
   text: {
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    width: 'auto',
+    color: '#fff',
+    padding: 16,
+    fontSize: 22,
   }
 });
