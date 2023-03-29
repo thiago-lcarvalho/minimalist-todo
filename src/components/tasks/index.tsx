a3f798f2ec16866a3595e6abe999f91c2c40c0f5
Thiago Carvalho <thiagoafonso07@hotmail.com>
2023-03-29 12:08:54 -0300

    📲

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -3,12 +3,14 @@ import { View, Text, Switch } from 'react-native';
 import { styles } from './styles';
 
 export function Tasks() {
+
 	const [isEnabled, setIsEnabled] = useState(false);
 	const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
 	return (
 		<View style={styles.View}>
-			<Text>placeholder</Text>
+			<Text style={[styles.text, isEnabled && styles.enabledText]}
+            >placeholder</Text>
 			<Switch
 				trackColor={{ false: '#A1A1A1', true: '#31CF67' }}
 				thumbColor={'#FFFFFF'}
@@ -18,4 +20,7 @@ export function Tasks() {
 			/>
 		</View>
 	);
+
+
 }
+
--- src/components/tasks/styles.ts
+++ src/components/tasks/styles.ts
@@ -2,6 +2,27 @@ import { StyleSheet, View } from 'react-native';
 
 export const styles = StyleSheet.create({
     View: {
-        backgroundColor: '#FFFFFF',
+        marginBottom: 32,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#1F1E25',
+        borderRadius: 8,
+        paddingRight: 8,
+        padding: 6,
+    },
+    text: {
+        color: '#FFFFFF',
+        flex: 1,
+        borderRadius: 5,
+        fontSize: 22,
+        marginRight: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+    },
+    enabledText: {
+        color: '#31CF67',
+        textDecorationLine: 'line-through',
     }
 });
