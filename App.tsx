b3787778120e778645340400dbd354db16f874c3
Thiago Carvalho <thiagoafonso07@hotmail.com>
2023-03-30 12:12:59 -0300

    📲

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { Home } from './src/screens/home';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 	return (
-		<Home/>
+		<>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			<Home />
+		</>
 	);
 }
